c69dc119da939350fb3dd789ffd187358af918c3
Dario <dariovieira777@gmail.com>
2019-04-23 14:37:13 +0200

    Tutorial on programatically making views

--- CanteenApp/CanteenApp/ViewController.swift
+++ CanteenApp/CanteenApp/ViewController.swift
@@ -2,7 +2,6 @@
 import UIKit
 import MSAL
 
-
 class ViewController: UIViewController, UITextFieldDelegate, URLSessionDelegate  {
     
     let tenantName = "cccbf502-6b91-40d6-be02-5ffa0eb711d6"
@@ -14,31 +13,97 @@ class ViewController: UIViewController, UITextFieldDelegate, URLSessionDelegate 
     
     var accessToken = String()
     
-    @IBOutlet weak var loggingText: UITextView!
+//    @IBOutlet weak var loggingText: UITextView!
+//
+//    @IBAction func authorizationButton(_ sender: UIButton) {
+//
+//        let authority = String(format: endpoint, tenantName, signInPolicy)
+//
+//        do {
+//            let application = try MSALPublicClientApplication.init(clientId: clientId, authority: authority)
+//
+//            application.acquireToken(forScopes: scopes) { (result, error) in
+//                    if  error == nil {
+//                        self.accessToken = (result?.accessToken)!
+//                        self.loggingText.text = "Access token is \(self.accessToken)"
+//                    } else {
+//                        self.loggingText.text = "Could not acquire token: \(error ?? "No error informarion" as! Error)"
+//                    }
+//            }
+//        } catch {
+//            self.loggingText.text = "Unable to create application \(error)"
+//        }
+//    }
     
-    @IBAction func authorizationButton(_ sender: UIButton) {
+    private let loginContentView:UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let unameTxtField:UITextField = {
+        let txtField = UITextField()
+        txtField.borderStyle = .roundedRect
+        txtField.translatesAutoresizingMaskIntoConstraints = false
+        txtField.placeholder = "Username"
+        return txtField
+    }()
+    
+    private let pwordTxtField:UITextField = {
+        let txtField = UITextField()
+        txtField.borderStyle = .roundedRect
+        txtField.translatesAutoresizingMaskIntoConstraints = false
+        txtField.placeholder = "Password"
+        return txtField
+    }()
+    
+    let btnLogin:UIButton = {
+        let btn = UIButton(type:.system)
+        btn.backgroundColor = .black
+        btn.setTitle("Login", for: .normal)
+        btn.tintColor = .white
+        btn.layer.cornerRadius = 5
+        btn.clipsToBounds = true
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+    
+    func setUpAutoLayout() {
+        loginContentView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+        loginContentView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+        loginContentView.heightAnchor.constraint(equalToConstant: view.frame.height/3).isActive = true
+        loginContentView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
-        let authority = String(format: endpoint, tenantName, signInPolicy)
+        unameTxtField.topAnchor.constraint(equalTo:loginContentView.topAnchor, constant:20).isActive = true
+        unameTxtField.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+        unameTxtField.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+        unameTxtField.heightAnchor.constraint(equalToConstant:50).isActive = true
         
-        do {
-            let application = try MSALPublicClientApplication.init(clientId: clientId, authority: authority)
-            
-            application.acquireToken(forScopes: scopes) { (result, error) in
-                    if  error == nil {
-                        self.accessToken = (result?.accessToken)!
-                        self.loggingText.text = "Access token is \(self.accessToken)"
-                    } else {
-                        self.loggingText.text = "Could not acquire token: \(error ?? "No error informarion" as! Error)"
-                    }
-            }
-        } catch {
-            self.loggingText.text = "Unable to create application \(error)"
-        }
+        pwordTxtField.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+        pwordTxtField.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+        pwordTxtField.topAnchor.constraint(equalTo:unameTxtField.bottomAnchor, constant:20).isActive = true
+        pwordTxtField.heightAnchor.constraint(equalToConstant:50).isActive = true
+        
+        btnLogin.topAnchor.constraint(equalTo:pwordTxtField.bottomAnchor, constant:20).isActive = true
+        btnLogin.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+        btnLogin.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+        btnLogin.heightAnchor.constraint(equalToConstant:50).isActive = true
+
     }
-
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        loginContentView.addSubview(unameTxtField)
+        loginContentView.addSubview(pwordTxtField)
+        loginContentView.addSubview(btnLogin)
+        
+        view.addSubview(loginContentView)
+        
+        setUpAutoLayout()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -55,4 +120,5 @@ class ViewController: UIViewController, UITextFieldDelegate, URLSessionDelegate 
         return nil
     }
 }
+
 
